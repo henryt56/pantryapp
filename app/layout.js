562028062f41f4a8c663f8c85cd0bb1c0c2ac315
henryt56 <henrytruong790@gmail.com>
2024-08-03 00:21:30 -0400

    title change

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import ThemeRegistry from './ThemeRegistry'
 
 export const metadata = {
-  title: 'Inventory Manager',
-  description: 'Inventory management application',
+  title: 'Pantry App',
+  description: 'Tracks contents in pantry',
 }
 
 export default function RootLayout({ children }) {
